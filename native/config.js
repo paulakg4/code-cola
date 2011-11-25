295aef87f82dca49fbf40f5095b6e5b022db08a9
paulakg4 <paulakg4@gmail.com>
2011-11-25 11:06:06 -0600

    Update native/config.js

--- native/config.js
+++ native/config.js
@@ -3,7 +3,7 @@ var YUI_config = {
 };
 YUI_config.groups['codecola'] = {
     combine: false,
-    base :'https://github.com/paulakg4/code-cola/',
+    base :'https://raw.github.com/paulakg4/code-cola/master/',
     modules:{
         'codecola-i18n': function(){
             var language = navigator.language.toLowerCase() == 'zh-cn'?'zh-CN':'en';
@@ -12,38 +12,38 @@ YUI_config.groups['codecola'] = {
             }
         }(),
         'codecola-color-css': {
-            fullpath: '/codecola/code-cola-widget/src/color/codecola-color.css',
+            fullpath: '/codecola/master/code-cola-widget/src/color/codecola-color.css',
             type: 'css'
         },
         'codecola-color': {
-            fullpath: '/codecola/code-cola-widget/src/color/codecola-color.js',
+            fullpath: '/codecola/master/code-cola-widget/src/color/codecola-color.js',
             requires: ['node', 'widget-base', 'codecola-color-css']
         },
         'codecola-degree-css': {
-            fullpath: '/codecola/code-cola-widget/src/degree/codecola-degree.css',
+            fullpath: '/codecola/master/code-cola-widget/src/degree/codecola-degree.css',
             type: 'css'
         },
         'codecola-degree': {
-            fullpath: '/codecola/code-cola-widget/src/degree/codecola-degree.js',
+            fullpath: '/codecola/master/code-cola-widget/src/degree/codecola-degree.js',
             requires: ['node', 'widget-base', 'codecola-degree-css']
         },
         'codecola-gradient-css': {
-            fullpath: '/codecola/code-cola-widget/src/gradient/codecola-gradient.css',
+            fullpath: '/codecola/master/code-cola-widget/src/gradient/codecola-gradient.css',
             type: 'css'
         },
         'codecola-gradient': {
-            fullpath: '/codecola/code-cola-widget/src/gradient/codecola-gradient.js',
+            fullpath: '/codecola/master/code-cola-widget/src/gradient/codecola-gradient.js',
             requires: ['codecola-color', 'node', 'widget-base', 'ua', 'codecola-gradient-css']
         },
         'codecola-css': {
-            fullpath: '/codecola/codecola.css',
+            fullpath: '/codecola/master/codecola.css',
             type: 'css'
         },
         'codecola-plugs': {
-            fullpath: '/codecola/plugin.js'
+            fullpath: '/codecola/master/plugin.js'
         },
         'codecola': {
-            fullpath: '/codecola/codecola.js',
+            fullpath: '/codecola/master/codecola.js',
             requires: ['codecola-i18n', 'codecola-plugs', 'codecola-color', 'codecola-gradient', 'codecola-degree', 'codecola-css', 'widget-base', 'node-base', 'event-base', 'io-base', 'dd-plugin', 'ua', 'json-parse']
         }
     }
